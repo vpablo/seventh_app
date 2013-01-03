644550bac5129b403eb5709e158adb1125be3cb7
vpablo <your.email@example.com>
2013-01-03 13:10:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeventhApp::Application.config.secret_token = '8726cb23a26b6d6c7c4fb978739915f4b92102d6b67cc1382f71e4994574d494d0ce0b41ed82e7186a42710bcf1a05eb0a5c0a48f3b5b68608579220fb48c7fe'
